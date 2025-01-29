923459b44c3eff513095000dbef05346f581e67c
AYANFE BABY DEV ✍️❣️ <neaterry6@gmail.com>
2025-01-29 13:33:49 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
+app.use(cors());
+app.use(express.json());
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('join', (data) => {
+        socket.userId = data.userId;
+        socket.nickname = data.nickname;
+        console.log(`${data.nickname} (${data.userId}) joined the chat`);
+    });
+
+    socket.on('sendMessage', (message) => {
+        io.emit('receiveMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
