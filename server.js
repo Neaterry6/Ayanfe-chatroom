42e2d4c8a61a70c672be80b2c9656fe0131b3105
AYANFE BABY DEV ✍️❣️ <neaterry6@gmail.com>
2025-01-29 13:56:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Serve the index.html file on root request
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('New client connected');
