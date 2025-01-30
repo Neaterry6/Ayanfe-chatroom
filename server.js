437a1bf18a32ee5eedf764fea431fefc3aa1812d
AYANFE BABY DEV ✍️❣️ <neaterry6@gmail.com>
2025-01-30 02:12:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the 'client' directory
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('client'));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+    console.log('New client connected');
 
     socket.on('join', (data) => {
-        socket.username = data.username;
-        console.log(`${socket.username} joined the chat`);
+        console.log(`${data.username} joined the chat`);
     });
 
     socket.on('chat', (data) => {
-        console.log('Message received:', data);
-        io.emit('chat', data); // Ensure this line is emitting the 'chat' event
+        io.emit('chat', data);
     });
 
     socket.on('fileUpload', (data) => {
-        console.log('File uploaded:', data);
         io.emit('chat', data);
     });
 
@@ -33,10 +30,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log(`${socket.username} disconnected`);
+        console.log('Client disconnected');
     });
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server running on port 3000');
 });
